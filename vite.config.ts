63b6250573ad29da07333326c4542f73ae56f52e
pratyaksh rai <pratyaksh.rai01@gmail.com>
2026-09-05 15:51:57 +0530

    fixing the  problme

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Mega_Notes/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
